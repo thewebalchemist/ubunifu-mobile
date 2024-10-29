22e534aa0470c4b39fe3ba806cddaf3eef2be9b3
berackkaunda <berackkaunda@gmail.com>
2024-10-29 18:13:19 +0000

    tuesday class

--- lib/screens/course.dart
+++ lib/screens/course.dart
@@ -26,6 +26,13 @@ class CoursePage extends StatelessWidget {
             ),
             SizedBox(height: 10),
             _buildCourseOutline(courseTitle),
+            SizedBox(height: 10),
+            Text(
+              'Contents:',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            SizedBox(height: 10),
+            _buildCourseContent(courseTitle)
           ],
         ),
       ),
@@ -36,30 +43,94 @@ class CoursePage extends StatelessWidget {
     List<String> outline;
     switch (course) {
       case 'Design Art':
-        outline = ['Color Theory', 'Typography', 'Layout Design', 'Digital Illustration'];
+        outline = [
+          'Color Theory',
+          'Typography',
+          'Layout Design',
+          'Digital Illustration'
+        ];
         break;
       case 'Programming':
-        outline = ['Basic Syntax', 'Data Structures', 'Algorithms', 'Object-Oriented Programming'];
+        outline = [
+          'Basic Syntax',
+          'Data Structures',
+          'Algorithms',
+          'Object-Oriented Programming'
+        ];
         break;
       case 'Mathematics':
         outline = ['Algebra', 'Geometry', 'Calculus', 'Statistics'];
         break;
       case 'Physics':
-        outline = ['Mechanics', 'Thermodynamics', 'Electromagnetism', 'Quantum Physics'];
+        outline = [
+          'Mechanics',
+          'Thermodynamics',
+          'Electromagnetism',
+          'Quantum Physics'
+        ];
         break;
       case 'App Development':
         outline = ['Flutter', 'Widgets', 'Firebase', 'Play Store'];
         break;
       default:
-        outline = ['Introduction', 'Core Concepts', 'Advanced Topics', 'Practical Applications'];
+        outline = [
+          'Introduction',
+          'Core Concepts',
+          'Advanced Topics',
+          'Practical Applications'
+        ];
     }
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: outline.map((topic) => Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Text('+ $topic', style: TextStyle(fontSize: 16)),
-      )).toList(),
+      children: outline
+          .map((topic) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('+ $topic', style: TextStyle(fontSize: 16)),
+              ))
+          .toList(),
     );
+  }
+
+  Widget _buildCourseContent(courseTitle) {
+    List<String> courseContent;
+    switch (courseTitle) {
+      case 'Design Art':
+        courseContent = ['This is the design art course contents, This is the design art course contents, This is the design art course contents'];
+        break;
+      case 'Programming':
+        courseContent = ['This is the programing course contents'];
+        break;
+
+      case 'Mathematics':
+        courseContent = ['This is the mathematics course contents'];
+        break;
+
+      case 'Physics':
+        courseContent = ['This is the physics course contents'];
+        break;
+
+      case 'App Development':
+        courseContent = ['This is the app development course contents'];
+        break;
+
+      default:
+        courseContent = ['This is the design art course contents'];
+        break;
+    }
+
+    return (Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: courseContent
+            .map((content) =>
+                Padding(padding: EdgeInsets.all(4), 
+                child: Text(
+                  content,
+                  style: TextStyle(
+                    fontSize: 20,
+                    
+                  )
+                )))
+            .toList()));
   }
 }
